2b1280341ac710d34fa03ed5fb20ce231cef3357
Pedro Caires <pedro.caires@iteris.com.br>
2021-04-14 21:27:28 -0300

    chore: readme do projeto com duvidas

---
+++ 2021/eterprise-connection/principal.py
@@ -0,0 +1,4 @@
+linha = input('Qual o nome da linha: ')
+quantidade_saida = int(input('Qual a quantidade de trens de manutenção: '))
+quantidade_entrada = int(input('Qual a quantidade de trens que vão estacionar: '))
+tempo_percurso_minutos = int()
